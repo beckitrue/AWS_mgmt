7b65bfdca35bb2c69ebb9076a0a52929143b15bc
Becki <becki@beckitrue.com>
2019-07-27 21:05:51 -0500

    changed logic and checked for no tags

--- stopEC2NonProd.py
+++ stopEC2NonProd.py
@@ -4,7 +4,7 @@ region = 'us-west-2'
 # assumes spot instances have been / are being terminated
 # before running this function - see
 # stopEC2Spot function
-# get running instances with Stage:prod tag
+# get running instances without the Stage:prod tag
 # shut down all instances that are non-prod
 
 
@@ -12,33 +12,6 @@ def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name=region)
 
     # get list of instances in a running state
-    # that have a Stage:prod key/value pair
-    # these instances will not be shut down
-    prod_instance_ids = []
-    prod_reservations = ec2.describe_instances(
-        Filters=[
-            {
-                'Name': 'instance-state-name',
-                'Values': [
-                    'running',
-                ]
-            },
-            {
-                'Name': 'tag:Stage',
-                'Values': [
-                    'prod',
-                ]
-            },
-        ],
-    ).get('Reservations', [])
-    # print(prod_reservations)
-    # create a list of instances with the Stage:prod tag
-    for prod_reservation in prod_reservations:
-        for instance in prod_reservation['Instances']:
-            prod_instance_ids.append(instance['InstanceId'])
-    # print(prod_instance_ids)
-
-    # get all running instances
     stop_instances = []
     reservations = ec2.describe_instances(
         Filters=[
@@ -46,15 +19,31 @@ def lambda_handler(event, context):
                 'Name': 'instance-state-name',
                 'Values': [
                     'running',
-                ]
+                ],
             },
         ],
     ).get('Reservations', [])
+
+    # get list of instances that do no have
+    # the Stage:prod tag
+    # these instances will be shutdown
     for reservation in reservations:
         for instance in reservation['Instances']:
-            if instance['InstanceId'] not in prod_instance_ids:
+            tags = {}
+            # check for the case where there are no tags
+            if instance.get('Tags') is not None:
+                for tag in instance['Tags']:
+                    tags[tag['Key']] = tag['Value']
+                if ('Stage' not in tags or
+                   tags['Stage'] not in ['prod', 'Prod']):
+                    print("instance " + instance['InstanceId'] +
+                          " does not have a Stage:prod tag")
+                    stop_instances.append(instance['InstanceId'])
+            # no tags, so add instance to stop list
+            else:
+                print("instance " + instance['InstanceId'] +
+                      " does not have a Stage:prod tag")
                 stop_instances.append(instance['InstanceId'])
-                print(instance['InstanceId'] + " added to stop list")
 
     # stop non-prod instances
     if not stop_instances:
