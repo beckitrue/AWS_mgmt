0c76683a5f1d889b30422480730ceb4d45aede09
Becki <becki@beckitrue.com>
2019-09-28 18:43:22 -0500

    combined code for non-prod and spot

--- stopEC2NonProd.py
+++ stopEC2NonProd.py
@@ -1,53 +1,68 @@
+from __future__ import print_function
+import json
 import boto3
+from botocore.exceptions import ClientError
 region = 'us-west-2'
+ec2 = boto3.resource('ec2', region_name=region)
 
-# assumes spot instances have been / are being terminated
-# before running this function - see
-# stopEC2Spot function
-# get running instances without the Stage:prod tag
-# shut down all instances that are non-prod
+# runs daily to check for instances that should
+# be shut down to save money
+# terminates spot instances
+# stops running instances without the Stage:prod tag
 
 
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2', region_name=region)
+    print("Received event: " + json.dumps(event, indent=2))
+    main()
 
-    # get list of instances in a running state
-    stop_instances = []
-    reservations = ec2.describe_instances(
-        Filters=[
-            {
-                'Name': 'instance-state-name',
-                'Values': [
-                    'running',
-                ],
-            },
-        ],
-    ).get('Reservations', [])
 
-    # get list of instances that do no have
-    # the Stage:prod tag
-    # these instances will be shutdown
-    for reservation in reservations:
-        for instance in reservation['Instances']:
-            tags = {}
-            # check for the case where there are no tags
-            if instance.get('Tags') is not None:
-                for tag in instance['Tags']:
-                    tags[tag['Key']] = tag['Value']
-                if ('Stage' not in tags or
-                   tags['Stage'] not in ['prod', 'Prod']):
-                    print("instance " + instance['InstanceId'] +
-                          " does not have a Stage:prod tag")
-                    stop_instances.append(instance['InstanceId'])
-            # no tags, so add instance to stop list
-            else:
-                print("instance " + instance['InstanceId'] +
-                      " does not have a Stage:prod tag")
-                stop_instances.append(instance['InstanceId'])
+def terminate_instance(instance_id):
+    # terminates spot instances
+    instance = ec2.Instance(instance_id)
+    print('terminating instance: ' + instance_id)
+    response = instance.terminate()
+    print(response)
+    return
 
-    # stop non-prod instances
-    if not stop_instances:
-        print('there are no running non-prod instances')
-    else:
-        ec2.stop_instances(InstanceIds=stop_instances)
-        print('stopped your instances: ' + str(stop_instances))
+
+def stop_instance(instance_id):
+    instance = ec2.Instance(instance_id)
+    print('stopping instance: ' + instance_id)
+    response = instance.stop()
+    print(response)
+    return
+
+
+# def lambda_handler(event, context):
+def search_instances():
+    try:
+        # get instances in the running state
+        # 1: look for spot instaances
+        # 2: instances that aren't tagged in Stage:prod
+        for instance in ec2.instances.all():
+            if instance.state['Name'] == 'running':
+                print('checking id: {0}'.format(instance.id))
+                if instance.instance_lifecycle == 'spot':
+                    print('spot instance ID: ' + instance.instance_id)
+                    terminate_instance(instance.instance_id)
+                else:
+                    # check tags and shutdown if not Stage:prod
+                    prod = {'Key': 'Stage', 'Value': 'prod'}
+                    if prod not in instance.tags:
+                        print('not tagged in Stage:prod')
+                        print(instance.tags)
+                        stop_instance(instance.id)
+                    else:
+                        # passes checks
+                        print('passes checks - keep running')
+
+    except ClientError as e:
+        print(e)
+
+
+def main():
+    search_instances()
+
+
+if __name__ == "__main__":
+    main()
